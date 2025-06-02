92eaaebb7c2d3b95d3bd32be3e5869f96b06ce77
Asjan Hasa <asjan.hasa@eng.it>
2025-06-02 12:30:18 +0200

    Initial commit: ASP.NET Core + React + Tailwind fullstack project

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
